1f697d50784be5201ce2b9b17045f2d7ad944d5f
Mritunjay021 <mrityurk21@gmail.com>
2024-02-27 17:24:08 +0530

    cpp

---
+++ 04_ControlStatements/03_forLoop.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i;
+    for(i = 1; i <= 10; i++){
+
+        if(i == 3){
+            continue;
+        }
+        
+        if(i == 7){
+            break;
+        }
+        cout << i << " ";
+    }
+
+    // for(int i = 100; i >= 1; i-=2){
+    //     cout << i << " ";
+
+    // }
+
+
+    //--------------------------------
+
+    // for(int i = 0; i < 3; i++){
+    //     for(int j = 0; j < 3; j++){
+    //         cout << i << " " << j << endl;
+    //     }
+    // }
+
+    // int row, col;
+
+    // cin >> row >> col;
+
+    // for(int i = 1; i <= row; i++){
+    //     for(int j = 1; j <= col; j++){
+    //         cout << "*";
+    //     } cout << endl;
+    // }
+
+    // for(int i = 1; i <= row; i++){
+    //     for(int j = 1; j <= col; j++){
+    //         if(i == 1 || i == row|| j == 1 || j == col){
+    //             cout << "*" << " ";
+    //         }
+    //         else{
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+}
